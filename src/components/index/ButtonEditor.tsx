a63fe18f98ef0224feda2dfdb51ce6760ef51b97
Lukas <info@brainlesslukas.com>
2026-06-08 18:42:28 +0200

    fix: background color

--- src/components/index/ButtonEditor.tsx
+++ src/components/index/ButtonEditor.tsx
@@ -2,7 +2,7 @@ import { ButtonGrid } from "./ButtonGrid.tsx"
 
 export const ButtonEditor = () => {
   return (
-    <div className="flex bg-[#222222] w-full h-screen text-white items-center justify-center">
+    <div className="flex bg-background w-full h-screen text-white items-center justify-center">
       <ButtonGrid />
     </div>
   )
