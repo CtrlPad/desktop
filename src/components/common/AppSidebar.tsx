94ae380f51f440ca02c6a9d50e092a36a6effcbd
Lukas <info@brainlesslukas.com>
2026-06-08 23:06:51 +0200

    feat: edit button action with dropdown menu (only ui)

--- src/components/common/AppSidebar.tsx
+++ src/components/common/AppSidebar.tsx
@@ -3,19 +3,56 @@ import {
   SidebarContent,
   SidebarFooter,
   SidebarGroup,
-  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuItem,
+  SidebarMenuButton,
+  SidebarMenuAction,
+  SidebarMenuSubItem,
+  SidebarMenuSub,
+  SidebarMenuSubButton
 } from "@/components/ui/sidebar"
+
+import { SquarePen, Cable } from "lucide-react"
 
 function AppSidebar() {
   return (
     <Sidebar>
-      <SidebarHeader />
       <SidebarContent>
-        <SidebarGroup />
-        <SidebarGroup />
+        <SidebarGroup>
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <SidebarMenuButton asChild>
+                <a href="/">Edit Button Layout</a>
+              </SidebarMenuButton>
+              <SidebarMenuAction>
+                <SquarePen />
+              </SidebarMenuAction>
+            </SidebarMenuItem>
+            <SidebarMenuItem>
+              <SidebarMenuButton asChild>
+                <p>Connection</p>
+              </SidebarMenuButton>
+              <SidebarMenuAction>
+                <Cable />
+              </SidebarMenuAction>
+              <SidebarMenuSub>
+                <SidebarMenuSubItem>
+                  <SidebarMenuSubButton asChild>
+                    <a href="/">Bluetooth (BLE-Server)</a>
+                  </SidebarMenuSubButton>
+                </SidebarMenuSubItem>
+                <SidebarMenuSubItem>
+                  <SidebarMenuSubButton asChild>
+                    <a href="/">Cable (Soon)</a>
+                  </SidebarMenuSubButton>
+                </SidebarMenuSubItem>
+              </SidebarMenuSub>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarGroup>
       </SidebarContent>
       <SidebarFooter />
-    </Sidebar>
+    </Sidebar >
   )
 }
 
--- src/components/index/ButtonGrid.tsx
+++ src/components/index/ButtonGrid.tsx
@@ -1,30 +1,72 @@
+import {
+  DropdownMenu,
+  DropdownMenuGroup,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuCheckboxItem
+} from "@/components/ui/dropdown-menu"
+
 interface Props {
+  id: number;
   label: string;
 }
 
 const buttons: Props[] = [
-  { id: "btn-0", label: "0" },
-  { id: "btn-1", label: "1" },
-  { id: "btn-2", label: "2" },
-  { id: "btn-3", label: "3" },
-  { id: "btn-4", label: "4" },
-  { id: "btn-5", label: "5" },
+  { id: 0, label: "0" },
+  { id: 1, label: "1" },
+  { id: 2, label: "2" },
+  { id: 3, label: "3" },
+  { id: 4, label: "4" },
+  { id: 5, label: "5" },
+]
+
+const applications = [
+  "Discord",
+  "Spotify",
+  "Bitwarden"
+]
+
+const media = [
+  "Volume Up",
+  "Volume Down",
+  "Mute Microphone"
 ]
 
 export const ButtonGrid = () => {
   return (
     <div className="grid grid-cols-3 gap-4 max-w-80">
       {buttons.map((button) => (
-        <Button key={button.id} label={button.label}/>
-      ))} 
+        <ButtonElement key={button.id} {...button} />
+      ))}
     </div>
   )
 }
 
-const Button = ({label}: Props) => {
+const ButtonElement = ({ label, id }: Props) => {
   return (
-    <div className="flex items-center justify-center border-2 border-white size-24 rounded-md">
-      <p>{label}</p>
-    </div>
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <div className="flex items-center justify-center border-3 border-primary size-24 rounded-md text-primary">
+          <p>{label}</p>
+        </div>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-64">
+        <DropdownMenuGroup>
+          <DropdownMenuLabel className="text-sm text-primary">Edit Button {id + 1}</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuLabel>Open Application</DropdownMenuLabel>
+          {applications.map((application) => (
+            <DropdownMenuCheckboxItem>{application}</DropdownMenuCheckboxItem>
+          ))}
+          <DropdownMenuSeparator />
+          <DropdownMenuLabel>Media</DropdownMenuLabel>
+          {media.map((media) => (
+            <DropdownMenuCheckboxItem>{media}</DropdownMenuCheckboxItem>
+          ))}
+        </DropdownMenuGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
   )
 }
