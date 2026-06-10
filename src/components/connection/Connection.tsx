94beae5093d962b28c456239ffd17e97acd8c9ff
Lukas <info@brainlesslukas.com>
2026-06-10 18:51:35 +0200

    fix: display icons instead of rssi numbers

--- src/components/connection/Connection.tsx
+++ src/components/connection/Connection.tsx
@@ -8,11 +8,17 @@ import {
   Table, TableBody, TableCaption, TableCell, TableRow
 } from "@/components/ui/table"
 import { Button } from "@/components/ui/button"
-import { BluetoothConnected, Bluetooth } from "lucide-react"
+import { BluetoothConnected, Bluetooth, SignalHigh, SignalMedium, SignalLow } from "lucide-react"
 import { useScanDevices } from "@/hooks/use-bluetooth"
 
 function Connection() {
   const { bleDevices } = useScanDevices()
+
+  const formatRssi = (rssi: number) => {
+    if (rssi >= -30) return <SignalHigh className="text-green-500" />
+    if (rssi >= -60) return <SignalMedium className="text-orange-500" />
+    if (rssi >= -90) return <SignalLow className="text-red-500" />
+  }
 
   return (
     <div className="flex w-full h-screen items-center justify-center">
@@ -28,7 +34,7 @@ function Connection() {
                 <TableRow>
                   <TableCell className="w-[10px]"><Bluetooth size={18} /></TableCell>
                   <TableCell>{device.name}</TableCell>
-                  <TableCell className="w-[10px]">{device.rssi}</TableCell>
+                  <TableCell className="w-[10px]">{formatRssi(device.rssi) || ""}</TableCell>
                   <TableCell className="w-[10px]"><Button><BluetoothConnected /></Button></TableCell>
                 </TableRow>
               ))}
