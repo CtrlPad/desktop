60f663e595916743ae6efea934c5005d5a36a77e
Lukas <info@brainlesslukas.com>
2026-06-11 17:53:32 +0200

    refactor: remove old layout editing

--- src/components/index/ButtonElement.tsx
+++ src/components/index/ButtonElement.tsx
@@ -1,72 +1,17 @@
-import {
-  DropdownMenu,
-  DropdownMenuGroup,
-  DropdownMenuContent,
-  DropdownMenuTrigger,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuCheckboxItem
-} from "@/components/ui/dropdown-menu"
-
-const applications: string[] = [
-  "Discord",
-  "Spotify",
-  "Bitwarden"
-]
-
-const media: string[] = [
-  "Volume Up",
-  "Volume Down",
-  "Mute Microphone"
-]
+interface Props {
+  button: Button
+}
 
 interface Button {
   id: number;
-  label: string;
+  label: string
 }
 
-interface Props {
-  button: Button;
-  currentAction: string;
-  onSelectAction: (buttonId: number, actionName: string) => void;
-}
-
-const ButtonElement = ({ button, currentAction, onSelectAction }: Props) => {
-  const handleItemClick = (item: string) => {
-    onSelectAction(button.id, item);
-  }
-
+const ButtonElement = ({ button }: Props) => {
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger>
-        <div className="flex items-center justify-center border-3 border-primary size-24 rounded-md text-primary">
-          <p>{button.label}</p>
-        </div>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-64">
-        <DropdownMenuGroup>
-          <DropdownMenuLabel className="text-sm text-primary">Edit Button {button.id + 1}</DropdownMenuLabel>
-          <DropdownMenuSeparator />
-          <DropdownMenuLabel>Open Application</DropdownMenuLabel>
-          {applications.map((item) => (
-            <DropdownMenuCheckboxItem
-              checked={currentAction === item}
-              onCheckedChange={() => handleItemClick(item)}>
-              {item}
-            </DropdownMenuCheckboxItem>
-          ))}
-          <DropdownMenuSeparator />
-          <DropdownMenuLabel>Media</DropdownMenuLabel>
-          {media.map((item) => (
-            <DropdownMenuCheckboxItem
-              checked={currentAction === item}
-              onCheckedChange={() => handleItemClick(item)}>
-              {item}
-            </DropdownMenuCheckboxItem>
-          ))}
-        </DropdownMenuGroup>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <div className="flex items-center justify-center border-3 border-primary size-24 rounded-md text-primary">
+      <p>{button.label}</p>
+    </div>
   )
 }
 
--- src/components/index/ButtonGrid.tsx
+++ src/components/index/ButtonGrid.tsx
@@ -1,5 +1,4 @@
 import { ButtonElement } from "./ButtonElement"
-import { useState } from "react"
 
 const buttons = [
   { id: 0, label: "0" },
@@ -10,20 +9,13 @@ const buttons = [
   { id: 5, label: "5" },
 ]
 
-interface Props {
-  selectedActions: Record<number, string>;
-  onSelectAction: (buttonId: number, actionName: string) => void;
-}
-
-export const ButtonGrid = ({ selectedActions, onSelectAction }: Props) => {
+export const ButtonGrid = () => {
   return (
     <div className="grid grid-cols-3 gap-4 max-w-80">
       {buttons.map((button) => (
         <ButtonElement
           key={button.id}
           button={button}
-          currentAction={selectedActions[button.id] || ""}
-          onSelectAction={onSelectAction}
         />
       ))}
     </div>
