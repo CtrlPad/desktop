fada396c7fbb7bac6341c693b3e14af0cab93469
Lukas <info@brainlesslukas.com>
2026-06-17 23:02:53 +0200

    feat: map all installed Applicatin to action bar

---
+++ src/components/common/Applications.tsx
@@ -0,0 +1,85 @@
+import {
+  SidebarMenu,
+  SidebarMenuItem,
+  SidebarMenuButton,
+  SidebarMenuAction,
+  SidebarMenuSubItem,
+  SidebarMenuSub,
+  SidebarMenuSubButton,
+} from "@/components/ui/sidebar"
+import { AppWindowMac, Disc3, GripVertical } from "lucide-react"
+import { useDraggable } from '@dnd-kit/react';
+import { useInstalledApps } from '@/hooks/use-applications';
+
+const applications = [
+  "Discord",
+  "Spotify",
+  "Bitwarden",
+  "Zen Browser",
+  "Ghostty",
+]
+
+const media = [
+  "Volume Up",
+  "Volume Down",
+  "Volume Mute",
+  "Microphone Toggle"
+]
+
+function DraggableSidebarItem({ id, children }: { id: string; children: React.ReactNode }) {
+  const { ref } = useDraggable({
+    id: id
+  });
+  return (
+    <div ref={ref}>
+      <SidebarMenuSubItem>
+        <SidebarMenuSubButton className="flex justify-between">
+          {children}
+          <GripVertical />
+        </SidebarMenuSubButton>
+      </SidebarMenuSubItem>
+    </div>
+  )
+}
+
+function Applications() {
+  const { availableApplications } = useInstalledApps();
+  return (
+    <SidebarMenu>
+      <SidebarMenuItem>
+        <SidebarMenuButton>
+          Applications
+        </SidebarMenuButton>
+        <SidebarMenuAction>
+          <AppWindowMac />
+        </SidebarMenuAction>
+      </SidebarMenuItem>
+      <SidebarMenuItem>
+        <SidebarMenuSub>
+          {availableApplications.map((item) => (
+            <DraggableSidebarItem id={"app-" + item} key={"app-" + item}>{item}</DraggableSidebarItem>
+          ))}
+        </SidebarMenuSub>
+      </SidebarMenuItem>
+      <SidebarMenuItem>
+        <SidebarMenuButton>
+          Media
+        </SidebarMenuButton>
+        <SidebarMenuAction>
+          <Disc3 />
+        </SidebarMenuAction>
+      </SidebarMenuItem>
+      <SidebarMenuItem>
+        <SidebarMenuSub>
+          {media.map((item) => (
+            <DraggableSidebarItem id={"media-" + item} key={"media-" + item}>{item}</DraggableSidebarItem>
+          ))}
+        </SidebarMenuSub>
+      </SidebarMenuItem>
+
+    </SidebarMenu>
+
+  )
+}
+
+export default Applications
---
+++ src/hooks/use-applications.ts
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react';
+import { platform } from '@tauri-apps/plugin-os';
+import { getApplications } from '@/utils/getXdgDataDirs';
+
+const useInstalledApps = () => {
+  const [availableApplications, setAvailableApplications] = useState<string[]>([])
+  const getInstalledApps = async () => {
+    try {
+      switch (platform()) {
+        case "linux":
+          const applications = await getApplications()
+          setAvailableApplications(applications)
+          break;
+        default:
+          console.log("Unsupported OS")
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    getInstalledApps();
+  }, [])
+
+  return { getInstalledApps, availableApplications }
+}
+
+export { useInstalledApps }
