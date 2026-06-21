d89b82b2f9eaa24caef97e684b4146b4a7905d79
Lukas <info@brainlesslukas.com>
2026-06-21 22:55:25 +0200

    feat: eslint prettier plugin

--- eslint.config.js
+++ eslint.config.js
@@ -3,11 +3,13 @@ import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 import json from "@eslint/json";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
   tseslint.configs.recommended,
+  eslintConfigPrettier,
   {
     files: ["**/*.{js,mjs,cjs,jsx,ts,mts,cts,tsx}"],
     ...pluginReact.configs.flat.recommended,
