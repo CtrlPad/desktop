7ac0e98bca82b3b4ba5750c4e508b15fb3e0004c
Lukas <info@brainlesslukas.com>
2026-08-30 10:49:35 +0200

    feat: render bg colors in combobox

--- src/components/index/ColorSelector.tsx
+++ src/components/index/ColorSelector.tsx
@@ -7,18 +7,32 @@ import {
   ComboboxInput,
 } from "@/components/ui/combobox";
 
-const colors = ["Red", "Blue", "Green", "Yellow"];
+type Color = {
+  label: string;
+  value: string;
+};
+
+const colors: Color[] = [
+  { label: "Red", value: "#c10007" },
+  { label: "Yellow", value: "#fcc800" },
+  { label: "Green", value: "#7ccf00" },
+  { label: "Blue", value: "#155dfc" },
+];
 
 export default function ColorSelector() {
   return (
-    <Combobox items={colors}>
+    <Combobox<Color> items={colors} itemToStringValue={(color) => color.label}>
       <ComboboxInput placeholder="Select Button Background-Color" />
       <ComboboxContent className="w-[var(--anchor-width)] max-w-[var(--available-width)]">
         <ComboboxEmpty>No colors found :(</ComboboxEmpty>
         <ComboboxList className="p-0">
           {(item) => (
-            <ComboboxItem key={item} value={item}>
-              {item}
+            <ComboboxItem key={item.label} value={item} className="flex gap-2">
+              <div
+                className="size-4 rounded-sm"
+                style={{ backgroundColor: item.value }}
+              />
+              <span>{item.label}</span>
             </ComboboxItem>
           )}
         </ComboboxList>
