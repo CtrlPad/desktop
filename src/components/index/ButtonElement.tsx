f9291ddbf201bd5ddf4f343a6ea807f3b399a1fb
Lukas <info@brainlesslukas.com>
2026-09-01 01:28:12 +0200

    feat: render preview of selected icon

--- src/components/index/ButtonElement.tsx
+++ src/components/index/ButtonElement.tsx
@@ -23,9 +23,12 @@ const ButtonElement = ({
       className={`relative flex items-center justify-center border-3 size-24 rounded-md text-primary transition-colors ${isDropTarget ? "border-green-500" : "border-primary"}`}
       ref={ref}
     >
-      <span className="text-muted-foreground">
-        {buttonIcon || buttonData || ""}
-      </span>
+      <span className="text-muted-foreground">{buttonData || ""}</span>
+      <span
+        className="[&>svg]:size-4 [&>svg]:fill-current absolute bottom-0 left-0 p-2"
+        aria-hidden="true"
+        dangerouslySetInnerHTML={{ __html: buttonIcon }}
+      />
       <div className="absolute bottom-0 right-0">
         <ButtonEditPopover buttonKey={buttonId} buttonIndex={buttonIndex} />
       </div>
