d68cc840f1804eaabb5b4d4bea0bb6c37a355b04
brainlesslukas <info@brainlesslukas.com>
2026-09-24 20:32:36 +0200

    feat: improve button preview

--- src/components/index/ButtonElement.tsx
+++ src/components/index/ButtonElement.tsx
@@ -14,20 +14,22 @@ const ButtonElement = ({ buttonKey, buttonData }: Props) => {
 
   return (
     <div
-      className={`relative flex items-center justify-center border-3 size-24 rounded-md text-primary transition-colors ${isDropTarget ? "border-green-500" : "border-primary"}`}
+      className={`relative flex items-center flex-col justify-center border-3 size-24 rounded-md text-primary transition-colors ${isDropTarget ? "border-green-500" : "border-primary"}`}
       style={{ backgroundColor: buttonData.buttonColor }}
       ref={ref}
     >
-      <span className="text-muted-foreground">{buttonData.name || ""}</span>
-
-      <div className="absolute bottom-0 left-0 p-2 flex flex-row gap-1 items-center">
-        <span
-          className="[&>svg]:size-4 [&>svg]:fill-current "
-          aria-hidden="true"
-          dangerouslySetInnerHTML={{ __html: buttonData.icon }}
-        />
-      </div>
-
+      {buttonData.icon && (
+        <div className="flex flex-row gap-1 items-center">
+          <span
+            className="[&>svg]:size-10 [&>svg]:fill-current "
+            aria-hidden="true"
+            dangerouslySetInnerHTML={{ __html: buttonData.icon }}
+          />
+        </div>
+      )}
+      <span className="text-muted-foreground text-sm">
+        {buttonData.name || buttonData.id + 1}
+      </span>
       <div className="absolute bottom-0 right-0">
         <ButtonEditPopover buttonKey={buttonKey} buttonIndex={buttonData.id} />
       </div>
